3d93248aec241f1b3ea8a8cb17fcea7c6ca25f5e
Ryan Large <ryan.large.96@googlemail.com>
2020-07-23 21:37:05 +0100

    Added Iteration count and stops loop once solved

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,20 +1,32 @@
 def bubble_sort(array)
   i = 0
-  j = 0
-  while j < array.length - 1
+  iterations = 0
+  new_array = []
+  last_array = []
+  sorted = false
+  while sorted == false
     while i < array.length - 1
       if array[i] > array[i + 1]
         array[i], array[i + 1] = array[i + 1], array[i]
       end
       i += 1
     end
-    i = 0
-    j += 1
+    new_array = array
+    if new_array == last_array
+      sorted = true
+    else
+      i = 0
+      iterations += 1
+      last_array = new_array.dup
+    end
   end
-  p array
+  print array
+  puts ", Iterations: #{iterations}"
 end
 
 bubble_sort([5, 4, 3, 2, 1])
-# => [1, 2, 3, 4, 5]
+# => [1, 2, 3, 4, 5], Iterations: 4
+bubble_sort([5, 1, 2, 3, 4])
+# => [1, 2, 3, 4, 5], Iterations: 1
 bubble_sort([4, 3, 78, 2, 0, 2])
-# => [0, 2, 2, 3, 4, 78]
+# => [0, 2, 2, 3, 4, 78], Iterations: 4
